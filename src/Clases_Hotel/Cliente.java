b976f45e5cffca02dc1e2f40869099b7abd33e1b
Pedro Eduardo De La Puente Rodriguez <pdr0014@gmail.com>
2026-05-05 09:22:24 +0200

    sapopeta

--- src/Clases_Hotel/Cliente.java
+++ src/Clases_Hotel/Cliente.java
@@ -2,5 +2,9 @@ package Clases_Hotel;
 
 public class Cliente {
 
-	//sapopeta
+	private String nombre="";
+	private String apellidos="";
+	private String DNI="";
+	private String email="";
+	private int telefeno=0;
 }
