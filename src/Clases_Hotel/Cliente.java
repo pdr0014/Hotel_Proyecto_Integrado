df2afd8f4efd6ce36678de2adf048e9dad8c8e25
Pedro Eduardo De La Puente Rodriguez <pdr0014@gmail.com>
2026-05-05 10:50:08 +0200

    setters y getters

--- src/Clases_Hotel/Cliente.java
+++ src/Clases_Hotel/Cliente.java
@@ -7,4 +7,39 @@ public class Cliente {
 	private String DNI="";
 	private String email="";
 	private int telefeno=0;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDNI() {
+		return DNI;
+	}
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefeno() {
+		return telefeno;
+	}
+	public void setTelefeno(int telefeno) {
+		this.telefeno = telefeno;
+	}
+	
+	
+	
 }
