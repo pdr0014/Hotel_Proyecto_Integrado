9773b759b125837c30f2e668e369aab93fd94211
Mauro Rodriguez <mrp0051@alu.medac.es>
2026-05-06 14:10:01 +0200

    Diseño de ventana1 y Prueba de botones.

--- src/Ventanas/Ventana_1.java
+++ src/Ventanas/Ventana_1.java
@@ -10,11 +10,16 @@ import javax.swing.JEditorPane;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ventana_1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+	private JButton btnNewButton;	
 	/**
 	 * Launch the application.
 	 */
@@ -37,6 +42,51 @@ public class Ventana_1 extends JFrame {
 	public Ventana_1() {
 		getContentPane().setBackground(new Color(235, 222, 207));
 		getContentPane().setLayout(null);
+		
+		
+		//TITULO INICIO
+		JLabel lblNewLabel = new JLabel("INICIO");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 29));
+		lblNewLabel.setBounds(172, 11, 90, 45);
+		getContentPane().add(lblNewLabel);
+		
+		
+		//BOTON AÑADIR CLIENTE	
+		btnNewButton = new JButton("Añadir Cliente");
+		btnNewButton.setBounds(167, 79, 99, 23);
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		        System.out.println("DEBUG: Click detectado"); // Esto DEBE salir en consola
+		        Ventana_AñadirCliente ventana = new Ventana_AñadirCliente();
+		        ventana.setVisible(true);
+		    }
+		});
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setBounds(167, 79, 99, 23);
+		getContentPane().add(btnNewButton);
+		
+		
+		//BOTON RESERVAR HABITACIÓN
+		JButton btnNewButton_1 = new JButton("Reservar Habitación");
+		btnNewButton_1.setBounds(152, 113, 129, 23);
+		getContentPane().add(btnNewButton_1);
+		
+		
+		//BOTON ELIMINAR RESERVA
+		JButton btnNewButton_2 = new JButton("Eliminar Reserva");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBounds(161, 181, 111, 23);
+		getContentPane().add(btnNewButton_2);
+		
+		
+		//BOTON ACTUALIZAR RESERVA
+		JButton btnNewButton_3 = new JButton("Actualizar Reserva");
+		btnNewButton_3.setBounds(155, 147, 123, 23);
+		getContentPane().add(btnNewButton_3);
 		
 	}
 }
---
+++ src/Ventanas/Ventana_AñadirCliente.java
@@ -0,0 +1,43 @@
+package Ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Ventana_AñadirCliente extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana_AñadirCliente frame = new Ventana_AñadirCliente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ventana_AñadirCliente() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
+
+}
