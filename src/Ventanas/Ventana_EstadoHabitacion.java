a38db2c88b4fe103f520e03ecc3c78d25a29ac3d
Pedro Eduardo De La Puente Rodriguez <pdr0014@gmail.com>
2026-05-14 12:58:53 +0200

    nuevas ventanas

---
+++ src/Ventanas/Ventana_EstadoHabitacion.java
@@ -0,0 +1,42 @@
+package Ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Ventana_EstadoHabitacion extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana_EstadoHabitacion frame = new Ventana_EstadoHabitacion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ventana_EstadoHabitacion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
+
+}
