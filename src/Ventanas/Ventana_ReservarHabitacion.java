7d20220c3030b689032a9fdbe0dade250ab50983
Pedro Eduardo De La Puente Rodriguez <pdr0014@gmail.com>
2026-05-14 13:21:39 +0200

    ventana reserva habitacion

--- src/Ventanas/Ventana_ReservarHabitacion.java
+++ src/Ventanas/Ventana_ReservarHabitacion.java
@@ -5,6 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ventana_ReservarHabitacion extends JFrame {
 
@@ -32,11 +40,42 @@ public class Ventana_ReservarHabitacion extends JFrame {
 	 */
 	public Ventana_ReservarHabitacion() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 491, 398);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Reservar Habitacion");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblNewLabel.setBounds(139, 11, 186, 51);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Numero de habitacion");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(157, 88, 168, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Tipo de habitacion");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(168, 158, 131, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(216, 113, 30, 22);
+		contentPane.add(comboBox);
+		
+		JButton btnNewButton = new JButton("Guardar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(179, 300, 120, 25);
+		contentPane.add(btnNewButton);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(216, 183, 30, 22);
+		contentPane.add(comboBox_1);
 
 	}
-
 }
